88a8ef92b45c7180a6cdefe038e2a887281d53fd
kxkm <t_bohl@kxkm.net>
2024-07-18 20:43:04 +0200

    conditional map link

--- www/app/js/pages/evenements.js
+++ www/app/js/pages/evenements.js
@@ -115,8 +115,6 @@ UTIL.setMapCoords = function(zoom, lat, lon, popupText) {
     });
     var p = L.marker([lat, lon], {icon: customIcon}).addTo(leafletMap).bindPopup(popupText)
     const btn = document.getElementById("event-location-coords-button");
-    // btn.href = "geo:" + lat + "," + lon;
-
     if (UTIL.getMobileOperatingSystem() == "iOS") 
         btn.href = "maps://maps.apple.com/?q="+lat+","+lon+"&z="+zoom;
     else
@@ -130,19 +128,19 @@ UTIL.setMapCoords = function(zoom, lat, lon, popupText) {
 UTIL.getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    // // Windows Phone must come first because its UA also contains "Android"
-    // if (/windows phone/i.test(userAgent)) {
-    //     return "Windows Phone";
-    // }
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
 
-    // if (/android/i.test(userAgent)) {
-    //     return "Android";
-    // }
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         return "iOS";
     }
 
-    return "Android";
+    return "unknown";
 }
