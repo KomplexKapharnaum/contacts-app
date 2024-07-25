e0a8ccfc84378f1f642acd956688791f422c665e
maigre <thomas@37m.gr>
2024-07-25 19:33:15 +0200

    msg send

--- server.js
+++ server.js
@@ -400,20 +400,20 @@ SOCKET.io.on('connection', (socket) => {
         );
         if (checked == true) 
         {
-          // db("users").select("is_connected", "phone").then((users) => {
-          //   users.forEach((u) => {
-          //     if (u.is_connected == 0) {
-          //       sendSMS([u.phone], "Nouveau message ! contacts.kxkm.net")
-          //     }
-          //   })
-          // })
           db("users").select("is_connected", "phone").then((users) => {
-            let phonelist = []
             users.forEach((u) => {
-              if (u.is_connected == 0) phoneList.push(u.phone)
+              if (u.is_connected == 0) {
+                sendSMS([u.phone], "Nouveau message ! contacts.kxkm.net")
+              }
             })
-            sendSMS(phonelist, "Nouveau message ! contacts.kxkm.net")
           })
+          // db("users").select("is_connected", "phone").then((users) => {
+          //   let phonelist = []
+          //   users.forEach((u) => {
+          //     if (u.is_connected == 0) phoneList.push(u.phone)
+          //   })
+          //   sendSMS(phonelist, "Nouveau message ! contacts.kxkm.net")
+          // })
 
         }
         SOCKET.io.emit('new_chatMessage', message, time_stamp, group)
