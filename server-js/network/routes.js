ba9e697f2d5fef7041ca10c765d7967f2107d773
maigre <thomas@37m.gr>
2025-01-21 17:20:06 +0100

    fix missing import

--- server-js/network/routes.js
+++ server-js/network/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { app } from '../core/server.js';
 import { query } from './query.js';
 import { __dirname } from '../../path.js';
+import fs from "fs";
 import police from '../core/police.js';
 
 app.use('/static', express.static('www'));
