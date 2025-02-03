58526fdc6b3b1d49832f6e6c026103ff4e011790
maigre <thomas@37m.gr>
2025-02-03 16:29:32 +0100

    vib simple

--- www/app/js/pages/profile.js
+++ www/app/js/pages/profile.js
@@ -185,24 +185,7 @@ function avatar_start_camera() {
                     
                     stream.getTracks().forEach(track => track.stop());
 
-                    if (navigator.vibrate) {
-
-                        if (typeof cordova !== 'undefined' && cordova.plugins.permissions)
-                            cordova.plugins.permissions.requestPermission(cordova.plugins.permissions.VIBRATE,
-                                (status) => {
-                                    if(status.hasPermission) {
-                                        console.log("Vibration permission granted")
-                                        let r = navigator.vibrate(1000);
-                                        console.log("Vibration", r)
-                                    }
-                                    else console.error("Vibration permission denied")
-                                }, () => {
-                                    console.error("Vibration permission denied")
-                                });
-
-
-                        
-                    }
+                    if (navigator.vibrate) navigator.vibrate(1000);
                     else console.error("Vibration not supported")
 
                 });
