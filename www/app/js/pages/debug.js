67735f38fb29b03496628c3f4be29d1deabcfd4f
EstebanVernet <contact@estebanver.net>
2025-03-11 13:16:08 +0100

    added debug page

--- www/app/js/pages/debug.js
+++ www/app/js/pages/debug.js
@@ -2,18 +2,34 @@ const debugTrigger = document.getElementById("debug-trigger")
 
 let debugPressTimer;
 
+let debugPressCount = 0;
+
+let debugPressTimeOut;
 debugTrigger.addEventListener("touchstart", () => {
-    console.log("touchstart")
-    debugPressTimer = setTimeout(() => {
-        console.log("Debug trigger pressed for 5 seconds");
-        // Add your logic here for when the button is pressed for 5 seconds
-    }, 5000);
+    console.log("debug trigger");
+    debugPressCount++;
+    console.log(debugPressCount);
+
+    if (debugPressCount === 10) {
+        PAGES.goto("debug");
+    }
+    clearTimeout(debugPressTimeOut)
+    debugPressTimeOut = setTimeout(() => { debugPressCount = 0; }, 500);
 });
 
-debugTrigger.addEventListener("touchend", () => {
-    clearTimeout(debugPressTimer);
-});
+const debugInputFile = document.getElementById("debug-input-file");
+const debugInputPreview = document.getElementById("debug-input-preview");
 
-debugTrigger.addEventListener("touchcancel", () => {
-    clearTimeout(debugPressTimer);
+debugInputFile.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+            debugInputPreview.innerHTML = "";
+            debugInputPreview.appendChild(img);
+        };
+        img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
 });
