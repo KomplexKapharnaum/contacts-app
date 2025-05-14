34b2af29460c7e2b4da0d3cdaf5736340f87a6f8
kxkm <t_bohl@kxkm.net>
2025-05-14 10:11:38 +0200

    shorter buddy dialogues

--- www/app/buddy_dialogues.js
+++ www/app/buddy_dialogues.js
@@ -1,40 +1,75 @@
+// var BUD_DIALS = {
+//     introduction: [
+//         "Bienvenue sur l'application CONTACTS ! Je suis Buddy, ton assistant virtuel qui pourra t'aider lors de ton aventure dans ce cyberespace !",
+//         "Cette application a pour but de t'informer des prochains rendez-vous spontanés du carnaval CONTACTS, ainsi que de te connecter au cyberespace.",
+//         "En t'inscrivant, tu acceptes que le contenu généré dans l'application puisse être utilisé dans le cadre du carnaval. Tous les contenus seront supprimés une fois l'événement terminé (Samedi 15 mars 2025).",
+//         "Bon séjour sur CONTACTS !"
+//     ],
+//     welcome: [
+//         "Dès qu'une icône rose apparaîtra dans le coin de ton écran, tu pourras appuyer dessus pour m'appeler. C'est aussi simple que ça !",
+//         "Dès que tu m'appelleras, je te donnerai des informations sur la page où tu te trouves. Au moins, il n'y aura plus de secret entre toi et cette application :)",
+//         /*"Tiens, au fait... Vu que c'est ta première fois, ça te dis de faire ton tout premier avatar ?",
+//         "Je te laisse naviguer dans ta page de profil et cliquer soit sur la notification tout en haut, soit dans 'générer un nouvel avatar'.",
+//         "Allez, je te laisse, bon séjour sur CONTACTS !"*/
+//     ],
+//     profile: [
+//         "Ici, c'est ta page de profil, c'est donc tout ce qui te concerne personnellement.",
+//         "Tu y retrouveras ton avatar, tes trophées et tes paramètres !",
+//         "Pour commencer, laisse-moi t'expliquer rapidement ce qu'est ton avatar.",
+//         "C'est une image qui te représentera et t'identifiera. Elle sera visible par tout le monde, et les membres du cyberespace pourront lui donner une note. Si ton avatar est très bien noté, tu pourras gagner des récompenses ou des améliorations, comme par exemple un avatar animé, quelle classe !",
+//         "Pour ce qui est des trophées, ils te permettront de gagner beaucoup de points, et donc d'apparaître dans le classement de ta tribu ! Je te laisserai cliquer sur les trophées et voir comment les débloquer.",
+//         "Je tiens à préciser que c'est aussi sur cette page où tu pourras voir tes précédentes notifications. Au moins, tu pourras les garder sous la main !"
+//     ],
+//     tribe: [
+//         "Bienvenue dans ta tribu, ton groupe, ta maison !",
+//         "C'est avec les questions que tu as fais précédemment que que nous t'avons assigné à ce groupe ! Au total, il y a 3 tribus.",
+//         "Vu que tu as rejoint ta tribu, tu peux maintenant avoir accès à la page tribu !",
+//         "Dans cette page, tu retrouveras le classement de ta tribu, défini par le nombre de points gagnés par chaque user. Toi aussi tu peux y apparaître, il te suffit de débloquer le plus de trophées et de participer sur l'application (chats, évènements etc...)",
+//         // "Tu peux aussi retrouver le chat (prononcé tchat') de ta tribu, que seul ton groupe peut voir et accéder. Vous pouvez, par exemple, trouver des stratégies pour gagner plus de points que les autres tribus, et être la meilleure de toutes !"
+//     ],
+//     cyberspace: [
+//         "L'application c'est bien, mais peut être que tu es un peu plus intéressé·e par les évènements !",
+//         "C'est ici que tu retrouveras les prochains rendez-vous, qui te seront annoncés juste avant son début. Le but : de la spontanéité dans la ville ! C'est un carnaval pirate après tout :)",
+//         "Sinon, c'est aussi ici que tu trouveras la messagerie du cyberespace : tu peux parler avec les autres users ou lire les récits de l'équipe CONTACTS !"
+//     ],
+//     profil_draw: [
+//         "Ton avatar va être généré par IA (Intelligence Artificielle). Pour commencer, prends un petit selfie !",
+//         "Ensuite, tu peux guider l'IA en dessinant sur ton selfie ! Si, par exemple, tu veux de grandes oreilles, dessine-les :)"
+//     ],
+//     tribe_join: [
+//         "Salut ! Je suis Buddy, ton assistant virtuel.",
+//         "Au sein de CONTACTS, tu fera parti d'une tribu. Pour cela, il te suffit de répondre aux questions qui vont t'être posé.",
+//         "Bon jeu :)"
+//     ]
+// }
+
 var BUD_DIALS = {
     introduction: [
-        "Bienvenue sur l'application CONTACTS ! Je suis Buddy, ton assistant virtuel qui pourra t'aider lors de ton aventure dans ce cyberespace !",
-        "Cette application a pour but de t'informer des prochains rendez-vous spontanés du carnaval CONTACTS, ainsi que de te connecter au cyberespace.",
-        "En t'inscrivant, tu acceptes que le contenu généré dans l'application puisse être utilisé dans le cadre du carnaval. Tous les contenus seront supprimés une fois l'événement terminé (Samedi 15 mars 2025).",
-        "Bon séjour sur CONTACTS !"
+        "Bienvenue dans CONTACTS ! Je suis Buddy, ton guide dans le cyberespace.",
+        "Ici, tu trouveras les prochains rendez-vous du carnaval et tu pourras te connecter au cyberespace.",
+        "En t'inscrivant, tu acceptes que le contenu généré soit utilisé pendant le carnaval. Tout sera supprimé après l'événement."
     ],
     welcome: [
-        "Dès qu'une icône rose apparaîtra dans le coin de ton écran, tu pourras appuyer dessus pour m'appeler. C'est aussi simple que ça !",
-        "Dès que tu m'appelleras, je te donnerai des informations sur la page où tu te trouves. Au moins, il n'y aura plus de secret entre toi et cette application :)",
-        /*"Tiens, au fait... Vu que c'est ta première fois, ça te dis de faire ton tout premier avatar ?",
-        "Je te laisse naviguer dans ta page de profil et cliquer soit sur la notification tout en haut, soit dans 'générer un nouvel avatar'.",
-        "Allez, je te laisse, bon séjour sur CONTACTS !"*/
+        "Dès qu'une icône rose apparaît dans le coin en haut à droite, appuie dessus pour m'appeler.",
+        "Quand tu me contactes, je t'explique la page où tu te trouves et ce que tu peux y faire.",
     ],
     profile: [
-        "Ici, c'est ta page de profil, c'est donc tout ce qui te concerne personnellement.",
-        "Tu y retrouveras ton avatar, tes trophées et tes paramètres !",
-        "Pour commencer, laisse-moi t'expliquer rapidement ce qu'est ton avatar.",
-        "C'est une image qui te représentera et t'identifiera. Elle sera visible par tout le monde, et les membres du cyberespace pourront lui donner une note. Si ton avatar est très bien noté, tu pourras gagner des récompenses ou des améliorations, comme par exemple un avatar animé, quelle classe !",
-        "Pour ce qui est des trophées, ils te permettront de gagner beaucoup de points, et donc d'apparaître dans le classement de ta tribu ! Je te laisserai cliquer sur les trophées et voir comment les débloquer.",
-        "Je tiens à préciser que c'est aussi sur cette page où tu pourras voir tes précédentes notifications. Au moins, tu pourras les garder sous la main !"
+        "Voici ta page de profil, tout ce qui te concerne. Tu y retrouves ton avatar, tes trophées, ta bio et tes paramètres.",
+        "Ton avatar, c’est l’image qui te représente. Il sera visible par tous et pourra être noté. Un super avatar peut te rapporter des récompenses, comme un avatar animé !",
+        "Les trophées te font gagner des points et grimper dans le classement de ta tribu. Clique dessus pour voir comment les débloquer.",
     ],
     tribe: [
-        "Bienvenue dans ta tribu, ton groupe, ta maison !",
-        "C'est avec les questions que tu as fais précédemment que que nous t'avons assigné à ce groupe ! Au total, il y a 3 tribus.",
-        "Vu que tu as rejoint ta tribu, tu peux maintenant avoir accès à la page tribu !",
-        "Dans cette page, tu retrouveras le classement de ta tribu, défini par le nombre de points gagnés par chaque user. Toi aussi tu peux y apparaître, il te suffit de débloquer le plus de trophées et de participer sur l'application (chats, évènements etc...)",
-        // "Tu peux aussi retrouver le chat (prononcé tchat') de ta tribu, que seul ton groupe peut voir et accéder. Vous pouvez, par exemple, trouver des stratégies pour gagner plus de points que les autres tribus, et être la meilleure de toutes !"
+        "Bienvenue dans ta tribu, ton groupe, ta maison ! Il y a 3 tribus au total.",
+        "Depuis cette page, tu vois le classement de ta tribu selon les points des membres. Gagne des trophées et participe pour y apparaître !",
+        "Tu pourras aussi y enregistrer ton cri de tribu et écouter ceux des autres membres.",
     ],
     cyberspace: [
-        "L'application c'est bien, mais peut être que tu es un peu plus intéressé·e par les évènements !",
-        "C'est ici que tu retrouveras les prochains rendez-vous, qui te seront annoncés juste avant son début. Le but : de la spontanéité dans la ville ! C'est un carnaval pirate après tout :)",
-        "Sinon, c'est aussi ici que tu trouveras la messagerie du cyberespace : tu peux parler avec les autres users ou lire les récits de l'équipe CONTACTS !"
+        "C’est ici que tu peux discuter avec tous les autres participants ou lire les récits générés par CONTACTS.",
+        "Envie d'évènements ? Tu trouves aussi ici les prochains rendez-vous, dont les lieus seront annoncés juste avant.."
     ],
     profil_draw: [
-        "Ton avatar va être généré par IA (Intelligence Artificielle). Pour commencer, prends un petit selfie !",
-        "Ensuite, tu peux guider l'IA en dessinant sur ton selfie ! Si, par exemple, tu veux de grandes oreilles, dessine-les :)"
+        "Ton avatar va être généré par IA (Intelligence Artificielle). Pour commencer, prends un selfie !",
+        "Tu peux ensuite dessiner sur ton selfie pour guider l’IA, par exemple des grandes oreilles :)"
     ],
     tribe_join: [
         "Salut ! Je suis Buddy, ton assistant virtuel.",
