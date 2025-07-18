7fb2a08ef332f8f316e16835e8d18b4bfae1dd6a
EstebanVernet <contact@estebanver.net>
2025-07-18 17:59:12 +0200

    regie bugfix

--- www/regie_carnaval/script.js
+++ www/regie_carnaval/script.js
@@ -129,7 +129,7 @@ function getEvents() {
                 const col = tribe=="" ? "white" : tribeColors[tribe-1]
                 btn.style.setProperty("--color", col)
                 btn.addEventListener("click", () => {
-                    ctrl(commands[cmd].json.name, JSON.parse(commands[cmd].data).args)
+                    ctrl(commands[cmd].json.name, commands[cmd].json.args)
                 })
                 document.getElementById('commands').appendChild(btn)           
             }
