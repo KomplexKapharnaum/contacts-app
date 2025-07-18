dbd6aa9541d60786c910adfe56605e7e54710617
maigre <thomas@37m.gr>
2025-07-18 12:09:04 +0000

    vocolor

--- www/app/js/pages/game-vocolor.js
+++ www/app/js/pages/game-vocolor.js
@@ -2,7 +2,7 @@ const VOCOLOR = new Vocolor({
     div: document.getElementById("voco-color"),
     color: "#00F",
     bufferSize: 100, // Size of the volume buffer
-    flashThreshold: 80 // Flash when volume is above this threshold
+    flashThreshold: 70 // Flash when volume is above this threshold
 });
 
 document.getElementById('voco-stop').addEventListener('click', () => {
